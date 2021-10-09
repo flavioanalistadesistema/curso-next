ecfdf8bf4408bd87dc66012fa4faeab5b4f3142a
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-10-09 12:31:24 -0300

    feat: JSX #3

---
+++ pages/basicos/jsx3.jsx
@@ -0,0 +1,15 @@
+//js
+
+export default function jsx3() {
+
+    const content = (
+        <div>
+            <h1>JSX #3</h1>
+        </div>
+    )
+        
+    
+    return content
+}
+
+// tipo do arquivo .jsx facilita a vscode a auto complete nas tags de HTML
