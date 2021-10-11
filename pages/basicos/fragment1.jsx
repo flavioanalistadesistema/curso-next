6b279469984ff6d5845d810a51f4e6d0ddb6e2c9
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-10-11 17:24:57 -0300

    feat: Uso de fragment #1 / #2

---
+++ pages/basicos/fragment1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function fragment1() {
+    return (
+        <React.Fragment>
+            <h1>titulo</h1>
+            <h2>subtitulo</h2>
+        </React.Fragment>
+    )
+}
+
+// quando declaramos dois elementos jsx é necessário um declaração de fragmento React
---
+++ pages/basicos/fragment2.jsx
@@ -0,0 +1,10 @@
+export default function fragment1() {
+    return (
+        <>
+            <h1>titulo</h1>
+            <h2>subtitulo</h2>
+        </>
+    )
+}
+
+// também podemos usar fragment dessa forma <> ...</>
