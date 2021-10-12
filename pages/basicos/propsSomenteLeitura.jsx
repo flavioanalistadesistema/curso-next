26066e8cd2503b4fdbe2801d68085f1be42b54f3
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-10-12 17:18:02 -0300

    feat: Props somente leitura

---
+++ pages/basicos/propsSomenteLeitura.jsx
@@ -0,0 +1,10 @@
+import SomaUm from "../../components/SomaUm";
+
+export default function propsSomenteLeitura() {
+    return <SomaUm numero={9} />
+}
+
+/*
+/ propriedade passada para nosso componente não pode ser alterado
+/ toda propriedade é somente leitura
+*/
