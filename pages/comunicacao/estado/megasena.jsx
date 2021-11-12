f83fa11012a8bf9d8dbe09ea3a32d4934a47fe7e
Flavio Santos <flavioanalistadesistema@gmail.com>
2021-11-11 22:48:40 -0300

    feat: desafio da megasena

---
+++ pages/comunicacao/estado/megasena.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+import { mega } from "../../../../function/mega"
+import ContadorDisplay from "../../../components/ContadorDisplay"
+
+export default function megasena() {
+    const [qtd, setQtd] = useState(6)
+    const [numeros, setNumeros] = useState(mega(qtd))
+
+    function render() {
+        return numeros.map(
+            numeros => <ContadorDisplay key={numeros} val={numeros} />
+        )
+    }
+
+    return (
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column"
+        }}>
+            <h1>Mega Sena</h1>
+            <div style={{
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "center"
+            }}>
+                {render()}
+            </div>
+            <input type="number" min="6" max="60" value={qtd} onChange={ev => setQtd(ev.target.value)} />
+            <button onClick={() => setNumeros(mega(qtd))}>Sorteio</button>
+        </div>
+    )
+}
